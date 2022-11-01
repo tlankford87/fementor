d0831fcd8d28ccba096bbbee37a791471df9868b
Thomas Roy Lankford <tlankford87@gmail.com>
2022-10-31 21:54:27 -0700

    added js to update card info on pressing confirm button

--- juniorLevel/interactive-card-details-form-main/interactive-card-details-form-main/js/main.js
+++ juniorLevel/interactive-card-details-form-main/interactive-card-details-form-main/js/main.js
@@ -1,6 +1,34 @@
-const cardNumber = document.getElementById('cardNumber')
-const input = document.getElementById('cNumber')
+document.querySelector('#confirm').addEventListener('click', changeCard)
 
-function changeNumber(){
-    cardNumber.textContent = input.value
+
+
+
+function changeCard(){
+    //cc number
+    const cardNumber = document.getElementById('cardNumber')
+    const cNumber = document.getElementById('cNumber').value
+    let cNumberSpaced = cNumber.match(/.{1,4}/g)
+    let cNumValue = cNumberSpaced.join(' ')
+    
+    cardNumber.textContent = cNumValue
+
+    //customer name
+    const name = document.getElementById('name')
+    const chName = document.getElementById('chName')
+
+    name.textContent = chName.value
+
+    //exp date
+    const experation = document.getElementById('experation')
+    const month = document.getElementById('expMonth')
+    const year = document.getElementById('expYear')
+
+    experation.textContent = `${month.value}/${year.value}`
+    
+    //cvc
+    const cardCVC = document.getElementById('cardCVC')
+    const cvc = document.getElementById('cvc')
+
+    cardCVC.textContent = cvc.value
+
 }
